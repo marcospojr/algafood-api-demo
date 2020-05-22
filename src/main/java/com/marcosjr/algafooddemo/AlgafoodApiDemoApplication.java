1fe856e97b2f82f431708d75c67f2c5b22508797
marcospojr <marcospojr13@gmail.com>
2020-05-22 18:41:11 -0300

    First commit

---
+++ src/main/java/com/marcosjr/algafooddemo/AlgafoodApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.marcosjr.algafooddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosjr/algafooddemo/AlgafoodApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosjr.algafooddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
