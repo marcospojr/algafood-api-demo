f8d3b039ba40ffafb1be928684c7c3af8ad3d966
marcospojr <marcospojr13@gmail.com>
2020-05-22 19:04:22 -0300

    Primeiro controller

---
+++ src/main/java/com/marcosjr/algafooddemo/PrimeiroController.java
@@ -0,0 +1,16 @@
+package com.marcosjr.algafooddemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PrimeiroController {
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String Hello() {
+		return "Hello!";
+	}
+
+}
